b3c9182bbb80a9afdcff310b321a9c0352e11328
Zaid Abdulameer <zabdulam33r@gmail.com>
2023-12-29 15:32:25 -0500

    Add server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    fs.readFile(__dirname + path, function(error, data) {  
+        if (error) {  
+            response.writeHead(404);  
+            response.write('This page does not exist');
+            response.end();  
+        } else {  
+            response.writeHead(200, {  
+                'Content-Type': 'text/html'
+            }); 
+            response.write(data);  
+            response.end();  
+        }  
+    }); 
+});  
+server.listen(8000);
